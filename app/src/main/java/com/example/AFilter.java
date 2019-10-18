33da1c3eda6ae6753de7ebfb77a02389eec26725
yangbinbing <yangbinbing@hachismart.com>
2019-10-18 18:40:00 +0800

    1：自定义相机

---
+++ app/src/main/java/com/example/AFilter.java
@@ -0,0 +1,259 @@
+package com.example;
+
+import android.content.res.Resources;
+import android.opengl.GLES20;
+import android.util.SparseArray;
+
+import com.example.util.Gl2Utils;
+
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+import java.util.Arrays;
+
+/**
+ * @author yangbinbing
+ * @date 2019/10/18
+ * @Description
+ */
+public abstract class AFilter {
+    private static final String TAG = "AFilter";
+    public static final float[] OM = Gl2Utils.getOriginalMatrix();
+
+    /**
+     * 程序句柄
+     */
+    protected int mProgram;
+
+    /**
+     * 定点句柄
+     */
+    protected int mHPosition;
+
+    /**
+     * 纹理坐标句柄
+     */
+    protected int mHCoord;
+
+    /**
+     * 总变换句柄
+     */
+    protected int mHMatrix;
+
+    /**
+     * 默认的纹理贴图
+     */
+    protected int mHTexture;
+
+    protected Resources mRes;
+
+    /**
+     * 定点坐标的buffer,里面存放定点坐标数据
+     */
+    protected FloatBuffer mVerBuffer;
+
+    /**
+     * 纹理坐标的buffer 里面存放纹理坐标
+     */
+    protected FloatBuffer mTexBuffer;
+
+    /**
+     * 索引坐标的buffer
+     */
+    protected ShortBuffer mIndexBuffer;
+
+    protected int mFlag = 0;
+
+    private float[] matrix = Arrays.copyOf(OM, 16);
+    private int textureType = 0;
+    private int textureId = 0;
+
+    private SparseArray<boolean[]> mBools;
+    private SparseArray<int[]> mInts;
+    private SparseArray<float[]> mFloats;
+
+    /**
+     * 图片的定点坐标
+     */
+    private float pos[] = {
+            -1.0f, 1.0f,
+            -1.0f, -1.0f,
+            1.0f, -1.0f,
+            1.0f, 1.0f,
+    };
+
+    /**
+     * 纹理坐标
+     */
+    private float[] coord = {
+            0.0f, 0.0f,
+            0.0f, 1.0f,
+            1.0f, 1.0f,
+            1.0f, 0.0f
+    };
+
+    public AFilter(Resources mRes) {
+        this.mRes = mRes;
+        initBuffer();
+    }
+
+    public void setMatrix(float[] matrix) {
+        this.matrix = matrix;
+    }
+
+    public float[] getMatrix() {
+        return matrix;
+    }
+
+    public final void setTextureType(int type) {
+        this.textureType = type;
+    }
+
+    public final int getTextureType() {
+        return textureType;
+    }
+
+    public final int getTextureId() {
+        return textureId;
+    }
+
+    public final void setTextureId(int textureId) {
+        this.textureId = textureId;
+    }
+
+    public void setFlag(int flag) {
+        this.mFlag = flag;
+    }
+
+    public int getFlag() {
+        return mFlag;
+    }
+
+    public void setFloat(int type, float... params) {
+        if (mFloats == null) {
+            mFloats = new SparseArray<>();
+        }
+        mFloats.put(type, params);
+    }
+
+    public void setInt(int type, int... params) {
+        if (mInts == null) {
+            mInts = new SparseArray<>();
+        }
+        mInts.put(type, params);
+    }
+
+    public void setBool(int type, boolean... params) {
+        if (mBools == null) {
+            mBools = new SparseArray<>();
+        }
+        mBools.put(type, params);
+    }
+
+    public boolean getBool(int type, int index) {
+        if (mBools == null) return false;
+        boolean[] b = mBools.get(type);
+        return !(b == null || b.length <= index) && b[index];
+    }
+
+    public int getInt(int type, int index) {
+        if (mInts == null) return 0;
+        int[] b = mInts.get(type);
+        if (b == null || b.length <= index) {
+            return 0;
+        }
+        return b[index];
+    }
+
+    public float getFloat(int type, int index) {
+        if (mFloats == null) return 0;
+        float[] b = mFloats.get(type);
+        if (b == null || b.length <= index) {
+            return 0;
+        }
+        return b[index];
+    }
+
+    public int getOutputTexture() {
+        return -1;
+    }
+
+    private void initBuffer() {
+        ByteBuffer a = ByteBuffer.allocateDirect(pos.length * 4);
+        a.order(ByteOrder.nativeOrder());
+        mVerBuffer = a.asFloatBuffer();
+        mVerBuffer.put(pos);
+        mVerBuffer.position(0);
+        ByteBuffer b = ByteBuffer.allocateDirect(coord.length * 4);
+        b.order(ByteOrder.nativeOrder());
+        mTexBuffer.put(coord);
+        mTexBuffer.position(0);
+    }
+
+    /**
+     * 实现此方法，完成程序的创建，可直接调用createProgram来实现
+     */
+    protected abstract void onCreate();
+
+    protected abstract void onSizeChanged(int width, int height);
+
+    protected final void createProgram(String vertex, String fragment) {
+        mProgram = Gl2Utils.createGlProgram(vertex, fragment);
+        mHPosition = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        mHCoord = GLES20.glGetAttribLocation(mProgram, "vCoord");
+        mHMatrix = GLES20.glGetUniformLocation(mProgram, "vMatrix");
+        mHTexture = GLES20.glGetUniformLocation(mProgram, "vTexture");
+    }
+
+    protected final void createProgramByAssetsFile(String vertex, String fragment) {
+        createProgram(Gl2Utils.uRes(mRes, vertex), Gl2Utils.uRes(mRes, fragment));
+    }
+
+    protected void onUseProgram() {
+        GLES20.glUseProgram(mProgram);
+    }
+
+    /**
+     *
+     */
+    protected void onDraw() {
+        GLES20.glEnableVertexAttribArray(mHPosition);
+        GLES20.glVertexAttribPointer(mHPosition, 2, GLES20.GL_FLOAT, false, 0, mVerBuffer);
+        GLES20.glEnableVertexAttribArray(mHCoord);
+        GLES20.glVertexAttribPointer(mHCoord, 2, GLES20.GL_FLOAT, false, 0, mTexBuffer);
+        /**
+         * 从第0个开始绘制，一共几个顶点
+         */
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, pos.length / 2);
+        GLES20.glDisableVertexAttribArray(mHPosition);
+        GLES20.glDisableVertexAttribArray(mHCoord);
+    }
+
+    protected void onClear() {
+        //黑色
+        GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+    }
+
+    /**
+     * 出入一个矩阵数据
+     */
+    protected void onSetExpandData() {
+        GLES20.glUniformMatrix4fv(mHMatrix, 1, false, matrix, 0);
+    }
+
+    /**
+     * // 绑定纹理
+     * GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+     * GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture);
+     */
+    protected void onBindTexture() {
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0 + textureType);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, getTextureId());
+        GLES20.glUniform1i(mHTexture, textureType);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/util/Gl2Utils.java
@@ -0,0 +1,252 @@
+/*
+ *
+ * FastDrawerHelper.java
+ * 
+ * Created by Wuwang on 2016/11/17
+ * Copyright © 2016年 深圳哎吖科技. All rights reserved.
+ */
+package com.example.util;
+
+import android.content.res.Resources;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+import android.util.Log;
+
+import java.io.InputStream;
+
+/**
+ * Description:
+ */
+public class Gl2Utils {
+
+    public static final String TAG="GLUtils";
+    public static boolean DEBUG=true;
+
+    /**
+     * 最大
+     */
+    public static final int TYPE_FITXY=0;
+    /**
+     * 中间裁剪
+     */
+    public static final int TYPE_CENTERCROP=1;
+    /**
+     * 居中
+     */
+    public static final int TYPE_CENTERINSIDE=2;
+    /**
+     * 从开始裁剪
+     */
+    public static final int TYPE_FITSTART=3;
+    /**
+     *
+     */
+    public static final int TYPE_FITEND=4;
+
+    private Gl2Utils(){
+
+    }
+
+    public static void getShowMatrix(float[] matrix,int imgWidth,int imgHeight,int viewWidth,int
+        viewHeight){
+        if(imgHeight>0&&imgWidth>0&&viewWidth>0&&viewHeight>0){
+            float sWhView=(float)viewWidth/viewHeight;
+            float sWhImg=(float)imgWidth/imgHeight;
+            float[] projection=new float[16];
+            float[] camera=new float[16];
+            if(sWhImg>sWhView){
+                Matrix.orthoM(projection,0,-sWhView/sWhImg,sWhView/sWhImg,-1,1,1,3);
+            }else{
+                Matrix.orthoM(projection,0,-1,1,-sWhImg/sWhView,sWhImg/sWhView,1,3);
+            }
+            Matrix.setLookAtM(camera,0,0,0,1,0,0,0,0,1,0);
+            Matrix.multiplyMM(matrix,0,projection,0,camera,0);
+        }
+    }
+
+    /**
+     * @param matrix 4*4的浮点矩阵
+     * @param type
+     * @param imgWidth
+     * @param imgHeight
+     * @param viewWidth
+     * @param viewHeight
+     */
+    public static void getMatrix(float[] matrix,int type,int imgWidth,int imgHeight,int viewWidth,
+                                 int viewHeight){
+        if(imgHeight>0&&imgWidth>0&&viewWidth>0&&viewHeight>0){
+            float[] projection=new float[16];
+            float[] camera=new float[16];
+            if(type==TYPE_FITXY){
+                Matrix.orthoM(projection,0,-1,1,-1,1,1,3);
+                Matrix.setLookAtM(camera,0,0,0,1,0,0,0,0,1,0);
+                //矩阵乘法Matrix.multiplyMM();
+                /**
+                 * result
+                 * 			The float array that holds the result. 保存结果的浮点数数组。
+                 * 		resultOffset
+                 * 			The offset into the result array where the result is stored.
+                 * 		lhs
+                 * 			The float array that holds the left-hand-side matrix. 保存左侧矩阵的浮点数数组。
+                 * 		lhsOffset
+                 * 			The offset into the lhs array where the lhs is stored
+                 * 		rhs
+                 * 			The float array that holds the right-hand-side matrix. 包含右侧矩阵的浮点数数组
+                 * 		rhsOffset
+                 * 			The offset into the rhs array where the rhs is stored.
+                 * ————————————————
+                 */
+                Matrix.multiplyMM(matrix,0,projection,0,camera,0);
+            }
+            float sWhView=(float)viewWidth/viewHeight;
+            float sWhImg=(float)imgWidth/imgHeight;
+            if(sWhImg>sWhView){
+                switch (type){
+                    case TYPE_CENTERCROP:
+                        Matrix.orthoM(projection,0,-sWhView/sWhImg,sWhView/sWhImg,-1,1,1,3);
+                        break;
+                    case TYPE_CENTERINSIDE:
+                        Matrix.orthoM(projection,0,-1,1,-sWhImg/sWhView,sWhImg/sWhView,1,3);
+                        break;
+                    case TYPE_FITSTART:
+                        Matrix.orthoM(projection,0,-1,1,1-2*sWhImg/sWhView,1,1,3);
+                        break;
+                    case TYPE_FITEND:
+                        Matrix.orthoM(projection,0,-1,1,-1,2*sWhImg/sWhView-1,1,3);
+                        break;
+                }
+            }else{
+                switch (type){
+                    case TYPE_CENTERCROP:
+                        Matrix.orthoM(projection,0,-1,1,-sWhImg/sWhView,sWhImg/sWhView,1,3);
+                        break;
+                    case TYPE_CENTERINSIDE:
+                        Matrix.orthoM(projection,0,-sWhView/sWhImg,sWhView/sWhImg,-1,1,1,3);
+                        break;
+                    case TYPE_FITSTART:
+                        Matrix.orthoM(projection,0,-1,2*sWhView/sWhImg-1,-1,1,1,3);
+                        break;
+                    case TYPE_FITEND:
+                        Matrix.orthoM(projection,0,1-2*sWhView/sWhImg,1,-1,1,1,3);
+                        break;
+                }
+            }
+            Matrix.setLookAtM(camera,0,0,0,1,0,0,0,0,1,0);
+            Matrix.multiplyMM(matrix,0,projection,0,camera,0);
+        }
+    }
+
+    public static void getCenterInsideMatrix(float[] matrix,int imgWidth,int imgHeight,int viewWidth,int
+            viewHeight){
+        if(imgHeight>0&&imgWidth>0&&viewWidth>0&&viewHeight>0){
+            float sWhView=(float)viewWidth/viewHeight;
+            float sWhImg=(float)imgWidth/imgHeight;
+            float[] projection=new float[16];
+            float[] camera=new float[16];
+            if(sWhImg>sWhView){
+                Matrix.orthoM(projection,0,-1,1,-sWhImg/sWhView,sWhImg/sWhView,1,3);
+            }else{
+                Matrix.orthoM(projection,0,-sWhView/sWhImg,sWhView/sWhImg,-1,1,1,3);
+            }
+            Matrix.setLookAtM(camera,0,0,0,1,0,0,0,0,1,0);
+            Matrix.multiplyMM(matrix,0,projection,0,camera,0);
+        }
+    }
+
+    public static float[] rotate(float[] m,float angle){
+        Matrix.rotateM(m,0,angle,0,0,1);
+        return m;
+    }
+
+    public static float[] flip(float[] m,boolean x,boolean y){
+        if(x||y){
+            Matrix.scaleM(m,0,x?-1:1,y?-1:1,1);
+        }
+        return m;
+    }
+
+    /**
+     * @return  原始矩阵|| 单位矩阵
+     */
+    public static float[] getOriginalMatrix(){
+        return new float[]{
+            1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            0,0,0,1
+        };
+    }
+
+    //通过路径加载Assets中的文本内容
+    public static String uRes(Resources mRes, String path){
+        StringBuilder result=new StringBuilder();
+        try{
+            InputStream is=mRes.getAssets().open(path);
+            int ch;
+            byte[] buffer=new byte[1024];
+            while (-1!=(ch=is.read(buffer))){
+                result.append(new String(buffer,0,ch));
+            }
+        }catch (Exception e){
+            return null;
+        }
+        return result.toString().replaceAll("\\r\\n","\n");
+    }
+
+    public static int createGlProgramByRes(Resources res, String vert, String frag){
+        return createGlProgram(uRes(res,vert),uRes(res,frag));
+    }
+
+    //创建GL程序
+    public static int createGlProgram(String vertexSource, String fragmentSource){
+        int vertex=loadShader(GLES20.GL_VERTEX_SHADER,vertexSource);
+        if(vertex==0)return 0;
+        int fragment=loadShader(GLES20.GL_FRAGMENT_SHADER,fragmentSource);
+        if(fragment==0)return 0;
+        int program= GLES20.glCreateProgram();
+        if(program!=0){
+            GLES20.glAttachShader(program,vertex);
+            GLES20.glAttachShader(program,fragment);
+            GLES20.glLinkProgram(program);
+            int[] linkStatus=new int[1];
+            GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS,linkStatus,0);
+            if(linkStatus[0]!= GLES20.GL_TRUE){
+                glError(1,"Could not link program:"+ GLES20.glGetProgramInfoLog(program));
+                GLES20.glDeleteProgram(program);
+                program=0;
+            }
+        }
+        return program;
+    }
+
+    //加载shader
+    public static int loadShader(int shaderType, String source){
+        int shader= GLES20.glCreateShader(shaderType);
+        if(0!=shader){
+            GLES20.glShaderSource(shader,source);
+            GLES20.glCompileShader(shader);
+            int[] compiled=new int[1];
+            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS,compiled,0);
+            if(compiled[0]==0){
+                glError(1,"Could not compile shader:"+shaderType);
+                glError(1,"GLES20 Error:"+ GLES20.glGetShaderInfoLog(shader));
+                GLES20.glDeleteShader(shader);
+                shader=0;
+            }
+        }
+        return shader;
+    }
+
+    public static void glError(int code, Object index){
+        if(DEBUG&&code!=0){
+            Log.e(TAG,"glError:"+code+"---"+index);
+        }
+    }
+
+
+    public static float[] scale(float[] m,float x,float y){
+        Matrix.scaleM(m,0,x,y,1);
+        return m;
+    }
+
+}
