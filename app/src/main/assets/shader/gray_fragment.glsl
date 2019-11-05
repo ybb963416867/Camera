25ca25bed7f243ddf73045c1614a7fa735a93c25
yangbinbing <yangbinbing@hachismart.com>
2019-11-05 10:45:46 +0800

    1：egl

--- app/src/main/assets/shader/gray_fragment.glsl
+++ app/src/main/assets/shader/gray_fragment.glsl
@@ -3,11 +3,8 @@ varying vec2 textureCoordinate;
 uniform  sampler2D vTexture;
 
 void main() {
-
     vec4 color=texture2D(vTexture, textureCoordinate);
     float rgb=color.g;
     vec4 c=vec4(rgb, rgb, rgb, color.a);
     gl_FragColor= c;
-
-
 }
