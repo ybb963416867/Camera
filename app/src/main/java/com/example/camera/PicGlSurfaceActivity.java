2c6b942314a6eeb7036bd07c366e981ec893bc8f
yangbinbing <yangbinbing@hachismart.com>
2019-11-07 10:56:39 +0800

    '删除.idea'

---
+++ app/src/main/java/com/example/camera/PicGlSurfaceActivity.java
@@ -0,0 +1,14 @@
+package com.example.camera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PicGlSurfaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pic_gl_surface);
+    }
+}
