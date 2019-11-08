b74d8700c960875dc07141bf8553b633543d76eb
yangbinbing <yangbinbing@hachismart.com>
2019-11-08 15:55:08 +0800

    1：视频录制

---
+++ app/src/main/java/com/example/camera/OpenglVideoRecodeActivity.java
@@ -0,0 +1,16 @@
+package com.example.camera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.base.BaseActivity;
+
+public class OpenglVideoRecodeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opengl_video_recode);
+    }
+}
