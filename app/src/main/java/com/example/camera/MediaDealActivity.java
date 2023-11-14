ad785c866711fca582467e8bd92b24984e747d9c
DESKTOP-RQFMMEM\dell <jinpeng123@163.com>
2023-11-14 09:15:14 +0800

    录屏

---
+++ app/src/main/java/com/example/camera/MediaDealActivity.java
@@ -0,0 +1,19 @@
+package com.example.camera;
+
+
+import android.os.Bundle;
+
+import com.example.base.BaseActivity;
+
+public class MediaDealActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_deal);
+
+
+
+
+    }
+}
